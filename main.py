086a78f1d5897d723ac80b417c008162355c0c1c
meatballs787 <jaden24dsa@gmail.com>
2026-01-27 23:46:54 -0600

    Second test commit with changes to class variables

--- main.py
+++ main.py
@@ -1,49 +1,39 @@
 class meatball():
-    def __init__(self, name, isize, istrength, ihp, idefense):
+    def __init__(self, name, istrength, ihp, idefense, class_type):
         self.name = name
-        self.size = isize
-        self.isize = isize
         self.strength = istrength
         self.istrength = istrength
         self.hp = ihp
-        self.ihp = ihp
+        self.imax_hp = ihp
+        self.max_hp = ihp
         self.defense = idefense
         self.idefense = idefense
+        self.class_type = class_type
+        # class type should start off as 0. 1 = warrior, 2 = thrower, 3 = tank 4 = bandit
+        self.level = 0
+        self.magic_mince = 0
+        self.mince = 0
 
     def die(self):
-        print(f"{self.name} has died./n {self.name} will respawn with  50% stats")
-        self.size *= 0.5
-        self.strength *= 0.5
-        self.hp *= 0.5
-        self.defense *= 0.5
-        if self.size < self.isize:
-            self.size = self.isize
-        if self.strength < self.istrength:
-            self.strength = self.istrength
-        if self.hp < self.ihp:
-            self.hp = self.ihp
-        if self.defense < self.idefense:
-            self.defense = self.idefense
-    def size_up(self, amount):
-        self.size += amount
+        print(f"{self.name} has died./n {self.name} will respawn with  50% health")
+        self.hp = self.max_hp // 2
     def strength_up(self, amount):
         self.strength += amount
     def defense_up(self, amount):
         self.defense += amount
     def heal(self, amount):
         self.hp += amount
-
-petJames = meatball("James", 10, 15, 100, 5)
-petJames.size_up(200)
-petJames.strength_up(500)
-petJames.defense_up(300)
-
-print(petJames.size)
-print(petJames.strength)
-print(petJames.hp)
-print(petJames.defense)
-petJames.die()
-print(petJames.size)
-print(petJames.strength)
-print(petJames.hp)
-print(petJames.defense)
+        if self.hp > self.max_hp:
+            self.hp = self.max_hp
+    def take_damage(self, amount):
+        damage_taken = amount - self.defense
+        if damage_taken < 0:
+            damage_taken = 0
+        self.hp -= damage_taken
+        if self.hp <= 0:
+            self.die()
+    def change_class(self, new_class):
+        self.class_type = new_class
+    def level_up(self):
+        self.level += 1
+        self.magic_mince +=1
